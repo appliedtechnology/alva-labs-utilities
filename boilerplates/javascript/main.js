984d0dd4591cf9431ea4e4c9b1a327726f02f4d8
Mathias Viklund <mathias.viklund@gmail.com>
2025-03-13 11:13:26 +0100

    refactor: update JavaScript boilerplate

---
+++ boilerplates/javascript/main.js
@@ -0,0 +1,21 @@
+// Write your solution in the area below
+// #region Solution
+
+
+
+// #endregion
+
+/// ** Please do not touch the code past this line! ** ///
+
+// #region Tests
+import { test } from "node:test";
+import assert from "node:assert/strict";
+
+test("this should pass", () => {
+  assert.equal(true, true);
+});
+// #endregion
+
+// #region Test Configurations
+Error.stackTraceLimit = 0;
+// #endregion
